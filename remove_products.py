86f9fd0a5436924d0814f8e8b48874c12764b364
Andrei Haidu <andrei.haidu@yahoo.com>
2019-01-02 10:03:21 +0100

    python script to remove actors from the world

---
+++ remove_products.py
@@ -0,0 +1,28 @@
+import random
+import unreal
+
+all_actors = unreal.EditorLevelLibrary.get_all_level_actors()
+
+unreal.log_warning("total actors: {}".format(len(all_actors)))
+
+#for act in all_actors:
+#	name = act.get_name()
+#	rand = random.randint(1,100)
+#	if(name.startswith("SM_ProductWithAN") and rand < 25):
+#		unreal.log_warning("delete {}".format(name))
+#		act.destroy_actor()
+
+with unreal.ScopedSlowTask(len(all_actors), "Removing Products Randomly") as slow_task:
+	slow_task.make_dialog(True)
+	frame = 1
+	for act in all_actors:
+		if slow_task.should_cancel():
+			break
+		frame += 1
+		slow_task.enter_progress_frame(frame)
+		
+		name = act.get_name()
+		rand = random.randint(1,100)
+		if(name.startswith("SM_ProductWithAN") and rand < 25):
+			unreal.log_warning("delete {}".format(name))
+			#act.destroy_actor()
